b4164f0d6424f1852727f7a856092a7b905742bc
rrabbi8 <76940637+rrabbi8@users.noreply.github.com>
2022-09-19 23:15:58 +1000

    PlusMinus Problem

---
+++ PlusMinus.java
@@ -0,0 +1,63 @@
+/* Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. 
+Print the decimal value of each fraction on a new line with 6 places after the decimal.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, 
+though answers with absolute error of up to 10^-4 are acceptable. */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+    // Write your code here
+    int pos = 0, neg = 0, zero = 0;
+    
+    for (int i=0; i<arr.size(); i++){
+        if (arr.get(i) > 0){
+            pos += 1;
+        } else if (arr.get(i) == 0){
+            zero += 1;
+        } else {
+            neg += 1;
+        }
+    }
+    
+    System.out.println(pos/(double)arr.size());
+    System.out.println(neg/(double)arr.size());
+    System.out.println(zero/(double)arr.size());
+    }
+
+}
+
+public class PlusMinus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
